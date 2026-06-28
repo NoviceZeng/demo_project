1253c7432048c5183b44f19a83fba7ded16c6403
Novice Zeng <zeng_novice@apple.com>
2026-06-29 01:21:02 +0800

    Delete repeated file.

--- demo-app-k8s-pipeline/scripts/helm_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#set -x
-# exit when encounter an  error
-set -e
-microsvc=$(echo "$1" | cut -d":" -f 1)
-harbor=$2
-build_number=$3
-bash demo-app-code/build-image.sh "${microsvc}" "${harbor}" "${build_number}" skip-package
